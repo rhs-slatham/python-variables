17c08d0c076b514bc313c57c2381cfda15123eee
rhs-slatham <74241071+rhs-slatham@users.noreply.github.com>
2020-11-10 10:26:36 +0000

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+# These are all valid variable names and assignment
+ 
+user_name = "studentguest"
+user_id = 1000
+ 
+# A variable's value can be changed after assignment
+ 
+points = 100
+points = 120
+
+print(points)
